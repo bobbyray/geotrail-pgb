3aa392e3f62036c65306f2601666d6113e4a2e1b
bobbyray <bobschomburg@gmail.com>
2016-11-20 08:48:47 -0800

    Set version to 1.1.021  11/20/2016_0846

--- www/js/Trail2.js
+++ www/js/Trail2.js
@@ -42,7 +42,7 @@ wigo_ws_GeoPathMap.OfflineParams = function () {
 // Object for View present by page.
 function wigo_ws_View() {
     // Release buld for Google Play on 09/20/2016 16:03
-    var sVersion = "1.1.021  11/19/2016_1417"; // Constant string for App version.
+    var sVersion = "1.1.021  11/20/2016_0846"; // Constant string for App version.
 
     // ** Events fired by the view for controller to handle.
     // Note: Controller needs to set the onHandler function.
