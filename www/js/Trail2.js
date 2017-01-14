61631967ed96acc0b00e020ffb85183c6249e035
bobbyray <bobschomburg@gmail.com>
2017-01-14 13:08:33 -0800

    Fix sizing for online trails bar.

--- www/js/Trail2.js
+++ www/js/Trail2.js
@@ -42,7 +42,7 @@ wigo_ws_GeoPathMap.OfflineParams = function () {
 // Object for View present by page.
 function wigo_ws_View() {
     // Release buld for Google Play on 09/20/2016 16:03
-    var sVersion = "1.1.022_20170114a"; // Constant string for App version.
+    var sVersion = "1.1.022_20170114_1220"; // Constant string for App version.
 
     // ** Events fired by the view for controller to handle.
     // Note: Controller needs to set the onHandler function.
