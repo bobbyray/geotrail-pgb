622a7ae8cfd99385f2b06934d8f0d9a4f874c65b
bobbyray <bobschomburg@gmail.com>
2017-05-24 16:37:23 -0700

    Set version to 1.1.026_20170523

--- www/js/Trail2.js
+++ www/js/Trail2.js
@@ -42,7 +42,7 @@ wigo_ws_GeoPathMap.OfflineParams = function () {
 // Object for View present by page.
 function wigo_ws_View() {
     // Work on RecordingTrail2 branch. Filter spurious record points.
-    var sVersion = "1.1.025_20170523"; // Constant string for App version.
+    var sVersion = "1.1.026_20170523"; // Constant string for App version.
 
     // ** Events fired by the view for controller to handle.
     // Note: Controller needs to set the onHandler function.
